75758966fd424f0902f6fc561935b5a6235eaf4a
amy <amyhoover@gmail.com>
2026-09-20 15:38:58 -0400

    cleaned the code a bit

--- minimalish_gif_maker.py
+++ minimalish_gif_maker.py
@@ -1,19 +1,18 @@
-
-
-
 import glob
 from PIL import Image
 import os
 
 
 def compile_pngs_to_gif(frames_dir, output_gif_path, fps=10):
-
+    # Get the files we want
     pattern = os.path.join(frames_dir, "frame_*.png")
     file_list = sorted(glob.glob(pattern))
+
+    # Save them as PIL Images
     frames = [Image.open(f) for f in file_list]
 
+    # Make the GIF
     frame_duration_ms = int(1000 / fps)
-
     frames[0].save(
         output_gif_path,
         save_all=True,
@@ -23,16 +22,18 @@ def compile_pngs_to_gif(frames_dir, output_gif_path, fps=10):
 
     print(f"Done! Saved GIF to: {output_gif_path}")
     
-    
-
-
 if __name__ == "__main__":
     run_folder="Run17-MaxSteps400"
-    #"Run14-MaxSteps400"
-    #"Run13-MaxSteps400"
-    #run_folder = "Run9-MaxSteps400"     #"Run7-MaxSteps400"
     FRAMES_DIRECTORY = f"lunar_lander_examples/{run_folder}"
     OUTPUT_FILE = f"lunar_lander_examples/{run_folder}.gif"
 
     compile_pngs_to_gif(FRAMES_DIRECTORY, OUTPUT_FILE, fps=30)
+
+    # List of Folders
+    #       "Run7-MaxSteps400"
+    #       "Run9-MaxSteps400"
+    #       "Run13-MaxSteps400"
+    #       "Run14-MaxSteps400"
+   
+       
 
